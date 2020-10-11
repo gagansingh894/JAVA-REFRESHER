50b02ea3b11c6b137924218d749cee1bd7df6d77
gagansingh894 <gds31.gagandeepsingh@gmail.com>
2020-10-12 00:54:45 +0530

    Static Code

---
+++ udacity_oop_inheritance/src/com/company/Person.java
@@ -0,0 +1,11 @@
+package com.company;
+
+public class Person {
+
+    public static int instanceCount;
+    public int localCount;
+    public Person() {
+        instanceCount ++;
+        localCount ++;
+    }
+}
