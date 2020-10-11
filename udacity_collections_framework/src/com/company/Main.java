74245085d4abb1cb62a3f7a41b3b5b29a9d8b9f7
gagansingh894 <gds31.gagandeepsingh@gmail.com>
2020-10-12 01:28:57 +0530

    Array and ArrayList Example Code

--- udacity_collections_framework/src/com/company/Main.java
+++ udacity_collections_framework/src/com/company/Main.java
@@ -1,8 +1,29 @@
 package com.company;
 
+import java.util.ArrayList;
+
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+
+    // Array Example
+        int[] myArray = new int[100];
+        myArray[0] = 100;
+        myArray[1] = 100;
+        System.out.println(myArray[3]);
+        System.out.println(myArray[0] + myArray[1]);
+
+	// ArrayList Example
+        ArrayList<Integer> grades = new ArrayList<>();
+    // Add example
+        grades.add(100);
+        grades.add(97);
+        grades.add(80);
+    //  General operation example
+        System.out.println(grades.get(0)); // Get element at index
+        System.out.println(grades.size()); // Get size of list
+        grades.remove(0); // Remove element at index 0
+        grades.clear(); // Clear all elements
     }
 }
