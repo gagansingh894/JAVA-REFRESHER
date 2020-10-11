d3087461cfb5e60f76ff5947082f78da8edc1865
gagansingh894 <gds31.gagandeepsingh@gmail.com>
2020-10-12 00:26:00 +0530

    Super Code

--- udacity_oop_inheritance/src/com/company/Pieces.java
+++ udacity_oop_inheritance/src/com/company/Pieces.java
@@ -7,9 +7,6 @@ package com.company;
 class King extends Piece {
 }
 
-class Rock extends Piece {
-}
-
 class Knight extends Piece {
 }
 
@@ -17,4 +14,19 @@ class Bishop extends Piece {
 }
 
 class Pawn extends Piece {
+}
+
+// Super
+class Rock extends Piece {
+
+  boolean isValidMove(Position newPosition) {
+
+   // Check super condition
+   if (!super.isValidMove(position)) {
+    return false;
+   }
+
+   // random
+   return true;
+  }
 }
